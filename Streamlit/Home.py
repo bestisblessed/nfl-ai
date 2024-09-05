9eb36a2b4b93aef4aee6967324a2ac2cddf52748
Tyler Durette <tyler.durette@gmail.com>
2024-09-04 21:48:18 -0400

    im dumb

--- Streamlit/Home.py
+++ Streamlit/Home.py
@@ -13,6 +13,10 @@ st.set_page_config(page_title="NFL AI", page_icon="🏈", layout="wide")
 st.title('NFL AI')
 st.write('Welcome to NFL AI')
 # st.write("[My Repo >](https://github.com/bestisblessed)")
+current_dir = os.path.dirname(os.path.abspath(__file__))
+justin_jefferson_path = os.path.join(current_dir, 'Streamlit/images/jettas.jpg')
+# st.image(justin_jefferson_path, use_column_width=True)
+st.image(justin_jefferson_path, width=300)
 
 # ---- Loading Data ---- #
 df_teams = pd.read_csv('Streamlit/data/Teams.csv')
@@ -117,6 +121,7 @@ image2_path = os.path.join(current_dir, 'Streamlit/images/dez.jpg')
 image3_path = os.path.join(current_dir, 'Streamlit/images/irvin.jpg')
 image4_path = os.path.join(current_dir, 'Streamlit/images/jettas.jpg')
 
+
 # Load the images using their absolute paths
 image1list = Image.open(image1_path)
 image2list = Image.open(image2_path)
@@ -129,9 +134,6 @@ with col2:
     st.image(image2list, use_column_width=True, caption="Image 2")
 with col3:
     st.image(image3list, use_column_width=True, caption="Image 3")
-
-# st.image(justin_jefferson_path, use_column_width=True)
-image4list = Image.open(image4_path)
 
 ### Video
 video1 = 'https://www.youtube.com/watch?v=K8RQkt3Ee0Y'
