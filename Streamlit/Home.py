6ba2e3642b9fcf8c61ee484f40ec385cb47a6665
Tyler Durette <tyler.durette@gmail.com>
2024-09-04 21:38:26 -0400

    im dumb

--- Streamlit/Home.py
+++ Streamlit/Home.py
@@ -106,9 +106,9 @@ with right_column:
 st.divider()
 # st.header('Galleria')
 
-image1list = Image.open('Streamlit/images/wp12999916-ceedee-lamb-cowboys-wallpapers Large.jpeg')
-image2list = Image.open('Streamlit/images/wp1872273-dez-bryant-wallpapers.jpg')
-image3list = Image.open('Streamlit/images/wp9604297-michael-irvin-wallpapers.jpg')
+image1list = Image.open('Streamlit/images/ceedee.jpg')
+image2list = Image.open('Streamlit/images/dez.jpg')
+image3list = Image.open('Streamlit/images/irvin.jpg')
 
 col1, col2, col3 = st.columns(3)
 with col1:
