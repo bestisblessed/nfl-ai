b6443c7102e033410e8943ab11cfc7b0e70c4a5e
Tyler Durette <tyler.durette@gmail.com>
2024-09-04 21:51:00 -0400

    im dumb

--- Streamlit/Home.py
+++ Streamlit/Home.py
@@ -13,7 +13,10 @@ st.set_page_config(page_title="NFL AI", page_icon="🏈", layout="wide")
 st.title('NFL AI')
 st.write('Welcome to NFL AI')
 # st.write("[My Repo >](https://github.com/bestisblessed)")
-current_dir = os.path.dirname(os.path.abspath(__file__))
+# current_dir = os.path.dirname(os.path.abspath(__file__))
+# justin_jefferson_path = os.path.join(current_dir, 'Streamlit/images/jettas.jpg')
+# st.image(justin_jefferson_path, use_column_width=True)
+# st.image(justin_jefferson_path, width=300)
 
 # ---- Loading Data ---- #
 df_teams = pd.read_csv('Streamlit/data/Teams.csv')
@@ -131,10 +134,6 @@ with col2:
     st.image(image2list, use_column_width=True, caption="Image 2")
 with col3:
     st.image(image3list, use_column_width=True, caption="Image 3")
-
-justin_jefferson_path = os.path.join(current_dir, 'Streamlit/images/jettas.jpg')
-# st.image(justin_jefferson_path, use_column_width=True)
-st.image(justin_jefferson_path, width=300)
 
 ### Video
 video1 = 'https://www.youtube.com/watch?v=K8RQkt3Ee0Y'
