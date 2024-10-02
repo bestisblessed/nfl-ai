31b37f50be0725f99227f5cd07a1a0142b2db973
bestisblessed <tyler.durette@gmail.com>
2024-10-02 03:14:45 -0400

    im dumb

--- Streamlit/pages/05_🏃🏾‍♂️_Player_Trends.py
+++ Streamlit/pages/05_🏃🏾‍♂️_Player_Trends.py
@@ -2,6 +2,8 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import os
+import streamlit as st
+from Home import df_teams, df_games, df_playerstats, df_team_game_logs, df_schedule_and_game_results
 
 # # Define the base directory
 # base_dir = os.path.abspath('../data')
@@ -16,15 +18,18 @@ st.title('Player Trends')
 # df_teams = pd.read_csv('./data/Teams.csv')
 # df_games = pd.read_csv('./data/Games.csv')
 # df_playerstats = pd.read_csv('./data/PlayerStats.csv')
-import streamlit as st
-from Home import df_teams, df_games, df_playerstats, df_team_game_logs, df_schedule_and_game_results
 
 # Store DataFrames in session state
-st.session_state['df_teams'] = df_teams
-st.session_state['df_games'] = df_games
-st.session_state['df_playerstats'] = df_playerstats
-st.session_state['df_team_game_logs'] = df_team_game_logs
-st.session_state['df_schedule_and_game_results'] = df_schedule_and_game_results
+# st.session_state['df_teams'] = df_teams
+# st.session_state['df_games'] = df_games
+# st.session_state['df_playerstats'] = df_playerstats
+# st.session_state['df_team_game_logs'] = df_team_game_logs
+# st.session_state['df_schedule_and_game_results'] = df_schedule_and_game_results
+df_teams = st.session_state['df_teams']
+df_games = st.session_state['df_games']
+df_playerstats = st.session_state['df_playerstats']
+df_team_game_logs = st.session_state['df_team_game_logs']
+df_schedule_and_game_results = st.session_state['df_schedule_and_game_results']
 
 st.divider()
 
