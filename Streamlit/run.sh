8fbc2d331916c3659decb19e780bdef66a1454ce
bestisblessed <tyler.durette@gmail.com>
2024-12-20 20:02:48 -0500

    im dumb

--- Streamlit/run.sh
+++ Streamlit/run.sh
@@ -7,4 +7,5 @@ mkdir -p data/odds/
 cp ../../odds-monitoring/NFL/Analysis/data/nfl_odds_movements.csv  data/odds/
 cp ../../odds-monitoring/NFL/Analysis/data/nfl_odds_movements_circa.csv  data/odds/
 cp ../../odds-monitoring/NFL/Analysis/data/odds/* data/odds/
+# rsync -av --progress "Trinity:/home/trinity/odds-monitoring/NFL/Scraping/data/odds/*" data/odds/ 
 streamlit run Home.py
