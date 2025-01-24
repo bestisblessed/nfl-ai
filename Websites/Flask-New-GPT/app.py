a7c81506f2f57c25e81e5de23cab35d5400b19ef
bestisblessed <tyler.durette@gmail.com>
2025-01-24 06:18:57 -0500

    OLD FLASK FROM M1

---
+++ Websites/Flask-New-GPT/app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/features')
+def features():
+    return render_template('features.html')
+
+@app.route('/pricing')
+def pricing():
+    return render_template('pricing.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
