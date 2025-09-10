84b0e9d4db855084111f7194547b0f4d22ecee81
bestisblessed <tyler.durette@gmail.com>
2025-09-10 13:55:47 -0400

    im dumb

--- Scrapers/scrape.sh
+++ Scrapers/scrape.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
+mkdir -p logs
+
+# Backup old log files if they exist (in the same directory)
+mv logs/log.txt logs/log.txt.bak
+mv logs/log-pfr.txt logs/log-pfr.txt.bak
+
 echo "================================================================================="
 echo "STARTING NFL DATA SCRAPER"
 echo "================================================================================="
-python -u ScraperFinal.py 2>&1 | tee log.txt
+python -u ScraperFinal.py 2>&1 | tee logs/log.txt
 
 echo ""
 echo "================================================================================="
 echo "PFR ONLY DATABASE"
 echo "================================================================================="
-python -u ScraperFinal-PFR.py 2>&1 | tee log-pfr.txt
+python -u ScraperFinal-PFR.py 2>&1 | tee logs/log-pfr.txt
