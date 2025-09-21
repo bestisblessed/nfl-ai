a317843e6732b6eb014fef26432f02a655612455
bestisblessed <tyler.durette@gmail.com>
2025-09-21 14:37:37 -0400

    im dumb

--- Scrapers/scrape_master_pfr.sh
+++ Scrapers/scrape_master_pfr.sh
@@ -5,7 +5,7 @@ echo "MASTER PFR-ONLY NFL DATA SCRAPER (2015-2024 seasons)"
 echo "================================================================================="
 
 # Run the master PFR scraper
-python -u ScraperMasterPFR.py
+python -u ScraperMasterPFR.py 2>&1 | tee logMasterPFR.log
 
 echo ""
 echo "================================================================================="
