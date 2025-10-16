3f650d11b5fcf023b19d07d1b87dc0260cd11895
bestisblessed <tyler.durette@gmail.com>
2025-10-16 14:47:05 -0400

    im dumb

--- Models/IN-PROGRESS/run.sh
+++ Models/IN-PROGRESS/run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-cp -r ../../Scrapers/data ./ # Copy data directory from Scrapers
-cp -f ../../Scrapers/nfl.db ./ # Copy nfl.db from Scrapers
-cp ../../Scrapers/final_data/*OCT_10* data
-
+rm -rf data
 rm -rf final_data_pfr
-cp -r ../../Scrapers/final_data_pfr .
+cp -r /Users/td/Code/nfl-ai/Scrapers/data ./ # Copy data directory from Scrapers
+cp -f /Users/td/Code/nfl-ai/Scrapers/nfl.db ./data/ # Copy nfl.db from Scrapers
+cp /Users/td/Code/nfl-ai/Scrapers/final_data/* data/
+cp -r /Users/td/Code/nfl-ai/Scrapers/final_data_pfr .
 
 rm -rf __pycache__
 echo "Copied data directory and nfl.db to current directory."
